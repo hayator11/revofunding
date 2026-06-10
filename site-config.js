472b4903f923bdd81def56d298f3b1c900eeeac0
hayator11 <ishishinokai@gmail.com>
2026-06-11 01:05:37 +0900

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -4,3 +4,6 @@ window.REVO_PROJECTS_DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-
 // 起案者作業ページからProjectsシートへ保存するApps Script WebアプリURL。
 // Apps ScriptでWebアプリをデプロイしたあと、発行されたURLをここに入れます。
 window.REVO_PROJECT_WORK_SAVE_ENDPOINT = "https://script.google.com/macros/s/AKfycbxVQlH1_-8etzBV8Yn4rkp645yLmCebyNRK_O0MzSZeJf1xW9DPMI3Xbtn_FI8QQMtA/exec";
+// 応援者マイページからSupportersシートの参加状況を確認するApps Script WebアプリURL。
+// 応援者フォーム側Apps Scriptをウェブアプリとしてデプロイしたあと、発行されたURLをここに入れます。
+window.REVO_SUPPORTER_STATUS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwXKd6zVVn80RYQy8Qms7EJUb2YrI6tmDjv_Ef-lCCg0B_lBWtLXxagvSnS4fllc-tf-w/exec";
